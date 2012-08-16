11fe13be07f4f1101ca053041a30ba9c1b867800
rosantiago <rosantiago@gmail.com>
2012-08-15 21:59:39 -0500

    jinja ok

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import url_for
+from jinja2 import Template 
+
 
 app = Flask(__name__)
 
+
+#enrutamientos
+
+#INDEX
 @app.route('/')
-def hello():
-	return "Meyahil dice hola desde Heroku con Flask"
+def index(name = None):
+	return render_template('content.html', name = 'Rodrigo')
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
+	#app.debug = True
 	app.run(host='0.0.0.0', port=port)
 
-	
