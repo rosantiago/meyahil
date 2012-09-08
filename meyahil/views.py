e9d75d288d7e704d85e65f796e0eb6fb192a9b57
rosantiago <rosantiago@rosantiago-inspiron.(none)>
2012-09-07 19:19:19 -0500

    sqleado

--- meyahil/views.py
+++ meyahil/views.py
@@ -1,4 +1,10 @@
 from meyahil import app
+from meyahil.database import db_session
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+
 
 @app.route('/')
 def index():
